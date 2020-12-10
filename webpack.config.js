af54448ca5176be52f9b39f650348557796d129d
aksmith5239 <aklobby@gmail.com>
2020-12-09 18:36:07 -0700

    updated package json

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,13 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname + '/dist'),
-    
+  },
+  module: {
+    rules: [
+        {
+            test: /\.jpg$/i
+        }
+    ]
   },
   plugins: [
       new webpack.ProvidePlugin({
